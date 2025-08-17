94fb841a491100893843817322cbe30fcaf4485f
Stelian DRAGNE <stelian.dragne@yahoo.com>
2025-08-17 14:06:27 +0300

    Intro to R

---
+++ CURS 1/02.variabile.R
@@ -0,0 +1,16 @@
+
+# Asignare a unei variabile
+a <- 3
+print(a)
+
+# Asignare inversa
+b <- 33
+print(b)
+
+c <- "Rosu"
+print(c)
+
+print(cat(b, c))
+
+print(cat(b, c, a))
+print(cat("b=", b, "c=", c, "a=", a))
---
+++ CURS 1/04.operatii.R
@@ -0,0 +1,34 @@
+
+a <- 10
+b <- 5
+
+print(a + b)  # Adunare
+print(a - b)  # Scadere
+print(a * b)  # Inmultire
+print(a / b)  # Impartire
+print(a %% b) # Restul impartirii
+print(a ^ b)  # Putere
+print(a %/% b) # Impartire intreaga
+print(a > b)  # Comparatie mai mare
+print(a < b)  # Comparatie mai mic
+print(a == b) # Comparatie egalitate
+print(a != b) # Comparatie diferit
+print(a >= b) # Comparatie mai mare sau egal
+print(a <= b) # Comparatie mai mic sau egal
+print(paste("Rezultatul adunarii este:", a + b))
+print(paste("Rezultatul scaderii este:", a - b))
+print(paste("Rezultatul inmultirii este:", a * b))
+print(paste("Rezultatul impartirii este:", a / b))
+print(paste("Restul impartirii este:", a %% b))
+print(paste("Puterea este:", a ^ b))
+print(paste("Impartirea intreaga este:", a %/% b))
+print(paste("Comparatia mai mare este:", a > b))
+print(paste("Comparatia mai mic este:", a < b))
+print(paste("Comparatia egalitate este:", a == b))
+print(paste("Comparatia diferit este:", a != b))
+print(paste("Comparatia mai mare sau egal este:", a >= b))
+print(paste("Comparatia mai mic sau egal este:", a <= b))
+print("Operatii matematice efectuate cu succes!")
+print("Acestea sunt operatiile matematice de baza in R.")
+print("Folosirea functiei paste pentru a afisa rezultatele operatiilor.")
+print("Acest script demonstreaza operatii matematice de baza in R.")
